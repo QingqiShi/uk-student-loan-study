893805fcc8866605a717083f36b37e79bc07c381
Qingqi Shi <hi@qingqi.dev>
2022-08-11 19:50:34 +0100

    build: setup firebase hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import App from './components/App';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDR92qhYCm5356Yuwustpl3wTl9n15voUQ',
+  authDomain: 'uk-student-loan-study.firebaseapp.com',
+  projectId: 'uk-student-loan-study',
+  storageBucket: 'uk-student-loan-study.appspot.com',
+  messagingSenderId: '941311784665',
+  appId: '1:941311784665:web:7329fc946da95d131ff0c8',
+  measurementId: 'G-C0MSJCEZVN',
+};
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
