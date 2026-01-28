fca043d52dc29fd62b12e812894677d0f766c615
Qingqi Shi <hi@qingqi.dev>
2026-01-28 23:33:56 +0000

    refactor: consolidate header and settings into unified floating header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,19 @@
-import { Header } from "./Header";
+import { FloatingHeader } from "./FloatingHeader";
 import { HeroSection } from "./HeroSection";
 import SecondaryCharts from "./SecondaryCharts";
-import AssumptionsFooter from "./AssumptionsFooter";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
+      <FloatingHeader />
       <main
         id="main-content"
-        className="mx-auto w-full max-w-4xl flex-1 space-y-8 px-4 py-6 pb-16 md:px-6 md:py-8"
+        className="mx-auto w-full max-w-4xl flex-1 space-y-8 px-4 py-6 md:px-6 md:py-8"
       >
         <HeroSection />
 
         <SecondaryCharts />
       </main>
-      <AssumptionsFooter />
     </div>
   );
 }
