b776526d0f02b304b17347c3e8f43db176865697
Qingqi Shi <hi@qingqi.dev>
2026-03-28 05:40:31 +0000

    feat: add security response headers

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,45 @@
 import type { NextConfig } from "next";
 
+// https://nextjs.org/docs/app/api-reference/config/next-config-js/headers#options
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: "frame-ancestors 'none'",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+  },
+];
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
   experimental: {
     viewTransition: true,
   },
+  headers: () =>
+    Promise.resolve([
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ]),
 };
 
 export default nextConfig;
